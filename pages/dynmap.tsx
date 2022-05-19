dc2febf251e99158c6d048b21427508e8a307ebc
Nudesuppe42 <jufo.theo@gmail.com>
2022-05-19 17:50:09 +0200

    Fixed Scaling of the Iframe

--- pages/dynmap.tsx
+++ pages/dynmap.tsx
@@ -1,36 +1,45 @@
 import {
-    Button,
-    Group,
-    Paper,
-    SimpleGrid,
-    Text,
-    TextInput,
-    Title,
-    useMantineTheme,
-  } from "@mantine/core";
+  Button,
+  Group,
+  Paper,
+  SimpleGrid,
+  Text,
+  TextInput,
+  Title,
+  useMantineTheme,
+} from "@mantine/core";
 
 import Page from "../components/Page";
 
 const DynmapPage = () => {
-    const theme = useMantineTheme();
-    return (
-      <Page>
-        <Paper
-            withBorder
-            radius="md"
-            p="xs"
-            style={{
-              marginBottom: theme.spacing.md,
-              height: "90vh"
-            }}
+  const theme = useMantineTheme();
+  return (
+    <Page>
+      <Paper
+        withBorder
+        radius="md"
+        p="xs"
+        style={{
+          marginBottom: theme.spacing.md,
+          height: "90vh",
+        }}
+      >
+        <Title
+          style={{
+            marginBottom: theme.spacing.md,
+          }}
         >
-        <Title>Dynmap of New York City</Title>
-        <iframe src="https://newyork.minefact.de/"
-                height={780} 
-                width={1565} />
-        </Paper>
-      </Page>
-    )
-}
+          Dynmap of New York City
+        </Title>
+        <iframe
+          src="https://newyork.minefact.de/"
+          height={"92.5%"}
+          width={"100%"}
+          style={{border:"none"}}
+        />
+      </Paper>
+    </Page>
+  );
+};
 
 export default DynmapPage;
