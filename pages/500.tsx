58ed5d4283fcc88f145a5aeb4b3100d121870860
Nudesuppe42 <jufo.theo@gmail.com>
2022-05-26 15:28:19 +0200

    Added static 404 and 500 pages

---
+++ pages/500.tsx
@@ -0,0 +1,84 @@
+import {
+  Button,
+  Container,
+  Group,
+  Text,
+  Title,
+  createStyles,
+} from "@mantine/core";
+
+import { useRouter } from "next/router";
+
+const useStyles = createStyles((theme) => ({
+  root: {
+    paddingTop: 80,
+    paddingBottom: 80,
+  },
+
+  label: {
+    textAlign: "center",
+    fontWeight: 900,
+    fontSize: 220,
+    lineHeight: 1,
+    marginBottom: theme.spacing.xl * 1.5,
+    color:
+      theme.colorScheme === "dark"
+        ? theme.colors.dark[4]
+        : theme.colors.gray[2],
+
+    [theme.fn.smallerThan("sm")]: {
+      fontSize: 120,
+    },
+  },
+
+  title: {
+    fontFamily: `Greycliff CF, ${theme.fontFamily}`,
+    textAlign: "center",
+    fontWeight: 900,
+    fontSize: 38,
+
+    [theme.fn.smallerThan("sm")]: {
+      fontSize: 32,
+    },
+  },
+
+  description: {
+    maxWidth: 500,
+    margin: "auto",
+    marginTop: theme.spacing.xl,
+    marginBottom: theme.spacing.xl * 1.5,
+  },
+}));
+
+function ErrorPage() {
+  const { classes } = useStyles();
+  const code = 500;
+  const status = {
+    code: 500,
+    title: "Our server decided to go get a 🥪.",
+    message:
+      "We're having some issues with our server, please try again later.",
+  };
+  const router = useRouter();
+  return (
+    <Container className={classes.root}>
+      <div className={classes.label}>{status.code}</div>
+      <Title className={classes.title}>{status.title}</Title>
+      <Text
+        color="dimmed"
+        size="lg"
+        align="center"
+        className={classes.description}
+      >
+        {" "}
+        {status.message}
+      </Text>
+      <Group position="center">
+        <Button variant="outline" size="md" onClick={() => router.back()}>
+          Go Back
+        </Button>
+      </Group>
+    </Container>
+  );
+}
+export default ErrorPage;
