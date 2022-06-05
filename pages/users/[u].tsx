d9524dbc0dd3d57c9e94ff33e1169600a3770ebf
Nudesuppe42 <jufo.theo@gmail.com>
2022-06-05 10:59:20 +0200

    Map on user page

--- pages/users/[u].tsx
+++ pages/users/[u].tsx
@@ -1,31 +1,92 @@
-import { Avatar, Center, Progress, Text, Title, useMantineTheme } from "@mantine/core";
+import {
+  Avatar,
+  Badge,
+  Button,
+  Center,
+  Group,
+  Progress,
+  Stack,
+  Text,
+  Title,
+  useMantineTheme,
+} from "@mantine/core";
+import { colorFromStatus, progressToColorName } from "../../utils/blockUtils";
 
+import Map from "../../components/Map";
 import Page from "../../components/Page";
-import { progressToColorName } from "../../utils/blockUtils";
+import { getRoleFromPermission } from "../../utils/hooks/usePermission";
+import { rankToColor } from "../../utils/userUtils";
 import { useRouter } from "next/router";
 import useSWR from "swr";
+import { useState } from "react";
 import useUser from "../../utils/hooks/useUser";
 
 const UserPage = () => {
   const router = useRouter();
   const theme = useMantineTheme();
+  const [selectedBlock, setSelectedBlock] = useState({
+    block: {
+      uid: 0,
+      id: 0,
+      district: -1,
+      status: -1,
+      progress: 0,
+      details: false,
+      builder: "",
+      completionDate: null,
+      area: "[]",
+    },
+    district: "",
+  });
   const { u } = router.query;
   const [user] = useUser();
   const { data } = useSWR(`/api/users/get/${u}`);
-  const { data:rndImage } = useSWR(`/api/admin/getRandomImage`);
+  const { data: claims } = useSWR(`/api/claims/${u}`);
+  const { data: rndImage } = useSWR(`/api/admin/getRandomImage`);
   var image = data?.image;
-  if(!image) {
+  if (!image) {
     image = rndImage?.link;
   }
-  console.log(data);
+  var claimsPolygons: any[] | null = null;
+  if (claims && !claimsPolygons) {
+    claimsPolygons = [];
+    claims?.claims.districts.map((district: any) => {
+      district.blocks?.map((block: any) => {
+        // @ts-ignore
+        claimsPolygons.push({
+          type: "polygon",
+          positions: block.area,
+          options: {
+            color: `${colorFromStatus(block.status, true)}FF`,
+            opacity:
+                    selectedBlock.block.uid != 0
+                      ? block.uid == selectedBlock.block.uid
+                        ? 1
+                        : 0.05
+                      : 0.5,
+          },
+          radius: 15,
+          tooltip:
+            district.name +
+            " #" +
+            block.id +
+            " | " +
+            (block.status == 1 ? block.builder : block.progress + "%"),
+          eventHandlers: {
+            click: () => {
+              setSelectedBlock({ block: block, district: district.name });
+            },
+          },
+        });
+      });
+    });
+  }
   return (
     <Page noMargin style={{ position: "relative" }} title={u?.toString()}>
       <div
         style={{
           backgroundColor: "white",
-          background: `url(${
-            image
-          }) center center / cover`,
+          background: `url(${image}) center center / cover`,
           width: "100%",
           height: "40vh",
         }}
@@ -43,18 +104,81 @@ const UserPage = () => {
         <Avatar size={theme.spacing.xl * 6} src={data?.picture} radius={2000}>
           {(u || [""]).toString().charAt(0)}
         </Avatar>
-        <Title
+        <Stack
+          align="flex-start"
+          spacing="xs"
           style={{
-            color: "white",
-            fontSize: theme.fontSizes.xl * 3,
             marginLeft: theme.spacing.xl,
-            userSelect: "none",
           }}
         >
-          {u}
-        </Title>
+          <Title
+            style={{
+              color: "white",
+              fontSize: theme.fontSizes.xl * 3,
+              userSelect: "none",
+            }}
+          >
+            {u}
+          </Title>
+          <Badge
+            style={{
+              backgroundColor: rankToColor(data?.rank) + "bb",
+              color: "#FFFFFF",
+              opacity: 1,
+              marginTop: theme.spacing.xs,
+            }}
+          >
+            {data?.rank}
+          </Badge>
+        </Stack>
       </Center>
-      <Progress value={21} color={progressToColorName(data?.progress)} radius={0} styles={{root:{backgroundColor:"#00000000"}}} style={{marginTop:-theme.spacing.md/4}}/>
+      <Progress
+        value={(claims?.claims.done / claims?.claims.total) * 100}
+        color={progressToColorName(
+          (claims?.claims.done / claims?.claims.total) * 100
+        )}
+        radius={0}
+        styles={{ root: { backgroundColor: "#00000000" } }}
+        style={{ marginTop: -theme.spacing.md / 4 }}
+      />
+      <div>
+        <Group
+          style={{
+            position: "absolute",
+            zIndex: 2,
+            bottom: theme.spacing.md,
+            width: "45vw",
+          }}
+          position="center"
+        >
+          {selectedBlock.block.uid != 0 && (
+            <Button
+              variant="filled"
+              color="gray"
+              radius="xl"
+              size="md"
+              onClick={() => {
+                router.push(
+                  "/districts/" + selectedBlock.district + "/" + selectedBlock.block.id
+                );
+              }}
+              style={{
+                boxShadow: theme.shadows.md,
+              }}
+            >
+              View Block Stats
+            </Button>
+          )}
+        </Group>
+        <Map
+          width="45vw"
+          height="53vh"
+          zoom={13}
+          polygon={{ data: data?.area || [] }}
+          mapStyle={{ zIndex: 0 }}
+          components={claimsPolygons}
+        ></Map>
+      </div>
     </Page>
   );
 };
