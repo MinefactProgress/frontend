9433f7641cc64975fd335a3e093b18353d56e1e4
Jannik0308 <jnkarger01@gmail.com>
2022-06-24 17:29:34 +0200

    Status filter for landmarks

--- pages/landmarks.tsx
+++ pages/landmarks.tsx
@@ -1,23 +1,32 @@
 import {
+  ActionIcon,
   Badge,
   Button,
   Grid,
   Group,
+  MediaQuery,
   Paper,
   ScrollArea,
   Table,
   Text,
+  TextInput,
+  Tooltip,
   useMantineTheme,
 } from "@mantine/core";
-import { Check, Cross } from "tabler-icons-react";
+import { Check, Cross, X } from "tabler-icons-react";
 import useSWR, { mutate } from "swr";
 
 import Map from "../components/Map";
 import Page from "../components/Page";
 import { Permissions } from "../utils/hooks/usePermission";
+import {
+  statusToNameLandmark,
+  statusToColorNameLandmark,
+} from "../utils/blockUtils";
 import { showNotification } from "@mantine/notifications";
 import { useState } from "react";
 import useUser from "../utils/hooks/useUser";
+import { useRouter } from "next/router";
 
 interface landmark {
   id: number;
@@ -31,11 +40,32 @@ interface landmark {
 }
 
 const LandmarksPage = () => {
+  const router = useRouter();
+  const [statusFilter, setStatusFilter] = useState<number | null | undefined>(
+    undefined
+  );
+  if (router.query.f && statusFilter === null) {
+    setStatusFilter(parseInt(router.query.f as string));
+  }
   const [user] = useUser();
   const { data } = useSWR("/api/landmarks/get");
   const { data: users } = useSWR("/api/users/get");
   const [selected, setSelected] = useState<landmark | null>(null);
   const theme = useMantineTheme();
+
+  const getLandmarkStatus = (landmark: landmark | null) => {
+    if (landmark === null) {
+      return null;
+    } else if (landmark.completed) {
+      return 3;
+    } else if (landmark.builder.length > 0) {
+      return 2;
+    } else if (landmark.requests.length > 0) {
+      return 1;
+    } else {
+      return 0;
+    }
+  };
 
   const handleRequest = (landmark: any) => {
     fetch(
@@ -188,9 +218,76 @@ const LandmarksPage = () => {
       <Grid>
         <Grid.Col sm={8}>
           <Paper withBorder radius="md" p="xs">
-            <Text color="dimmed" size="xs" transform="uppercase" weight={700}>
-              Landmarks
-            </Text>
+            <div style={{ width: "100%" }}>
+              <Text
+                color="dimmed"
+                size="xs"
+                transform="uppercase"
+                weight={700}
+                style={{ display: "inline-block" }}
+              >
+                Landmarks
+              </Text>
+              <MediaQuery smallerThan={"sm"} styles={{ display: "none" }}>
+                <Group style={{ float: "right" }}>
+                  <Text
+                    color="dimmed"
+                    size="xs"
+                    transform="uppercase"
+                    weight={700}
+                  >
+                    Filter
+                  </Text>
+                  {user.uid > 0 ? (
+                    <Badge
+                      variant={statusFilter === 4 ? "filled" : "outline"}
+                      onClick={(e: any) => {
+                        setStatusFilter(4);
+                        router.push("/landmarks?f=4");
+                      }}
+                    >
+                      My Claims
+                    </Badge>
+                  ) : null}
+                  {[3, 2, 1, 0].map((status) => (
+                    <Badge
+                      key={status}
+                      color={statusToColorNameLandmark(status)}
+                      variant={statusFilter === status ? "filled" : "outline"}
+                      onClick={(e: any) => {
+                        setStatusFilter(status);
+                        if (getLandmarkStatus(selected) !== status) {
+                          setSelected(null);
+                        }
+                        router.push("/landmarks?f=" + status);
+                      }}
+                    >
+                      {statusToNameLandmark(status)}
+                    </Badge>
+                  ))}
+                  {statusFilter !== undefined ? (
+                    <Tooltip
+                      label="Clear Filter"
+                      placement="end"
+                      withArrow
+                      position="bottom"
+                    >
+                      <ActionIcon
+                        size="xs"
+                        radius="xl"
+                        variant="outline"
+                        onClick={() => {
+                          setStatusFilter(undefined);
+                          router.push("/landmarks");
+                        }}
+                      >
+                        <X size={16} />
+                      </ActionIcon>
+                    </Tooltip>
+                  ) : null}
+                </Group>
+              </MediaQuery>
+            </div>
             <ScrollArea style={{ height: "75vh" }}>
               <Table highlightOnHover>
                 <thead>
@@ -295,93 +392,126 @@ const LandmarksPage = () => {
                     </tr>
                   )}
                   {data
-                    ? data?.map((landmark: any) => (
-                        <tr key={landmark.id}>
-                          <td width="20%">{landmark.name}</td>
-                          <td>
-                            {landmark.completed ? (
-                              <Badge color="green">Done</Badge>
-                            ) : landmark.builder.length > 0 ? (
-                              <Badge color="orange">Claimed</Badge>
-                            ) : landmark.requests.length > 0 ? (
-                              <Badge color="cyan">Requested</Badge>
-                            ) : (
-                              <Badge color="red">Not Claimed</Badge>
-                            )}
-                          </td>
-                          <td>
-                            {landmark.builder.length > 0 ? (
-                              <Group>
-                                {landmark.builder.map((u: any) => (
-                                  <Badge
-                                    key={u}
-                                    variant="outline"
-                                    onClick={() =>
-                                      handleRemoveBuilder(
-                                        landmark,
-                                        users.find((e: any) => e.username === u)
-                                          .uid
-                                      )
-                                    }
-                                  >
-                                    {u}
-                                  </Badge>
-                                ))}
-                              </Group>
-                            ) : (
-                              "---"
-                            )}
-                          </td>
-                          <td>
-                            {landmark.requests.length > 0 ? (
-                              <Group>
-                                {landmark.requests.map((u: any) => (
-                                  <Badge
-                                    key={u}
-                                    variant="outline"
-                                    onClick={() =>
-                                      handleAddBuilder(
-                                        landmark,
-                                        users.find((e: any) => e.username === u)
-                                          .uid
-                                      )
-                                    }
-                                  >
-                                    {u}
-                                  </Badge>
-                                ))}
-                              </Group>
-                            ) : (
-                              "---"
-                            )}
-                          </td>
-                          <td>
-                            {landmark.requests.includes(user?.username) ? (
-                              <Button
-                                color="red"
-                                disabled={
-                                  landmark.completed ||
-                                  landmark.builder.length > 0
-                                }
-                                onClick={() => handleUnrequest(landmark)}
-                              >
-                                Unapply
-                              </Button>
-                            ) : (
-                              <Button
-                                color="green"
-                                disabled={
-                                  landmark.completed ||
-                                  landmark.builder.length > 0
-                                }
-                                onClick={() => handleRequest(landmark)}
-                              >
-                                Apply
-                              </Button>
-                            )}
-                          </td>
-                        </tr>
-                      ))
+                    ? data
+                        ?.filter((landmark: any) => {
+                          switch (statusFilter) {
+                            case 0:
+                              return (
+                                landmark.requests.length === 0 &&
+                                landmark.builder.length === 0 &&
+                                !landmark.completed
+                              );
+                            case 1:
+                              return (
+                                landmark.requests.length > 0 &&
+                                landmark.builder.length === 0 &&
+                                !landmark.completed
+                              );
+                            case 2:
+                              return (
+                                landmark.builder.length > 0 &&
+                                !landmark.completed
+                              );
+                            case 3:
+                              return landmark.completed;
+                            case 4:
+                              return (
+                                landmark.builder.includes(user.username) ||
+                                landmark.requests.includes(user.username)
+                              );
+                            default:
+                              return true;
+                          }
+                        })
+                        .map((landmark: any) => (
+                          <tr key={landmark.id}>
+                            <td width="20%">{landmark.name}</td>
+                            <td>
+                              {landmark.completed ? (
+                                <Badge color="green">Done</Badge>
+                              ) : landmark.builder.length > 0 ? (
+                                <Badge color="orange">Claimed</Badge>
+                              ) : landmark.requests.length > 0 ? (
+                                <Badge color="cyan">Requested</Badge>
+                              ) : (
+                                <Badge color="red">Not Claimed</Badge>
+                              )}
+                            </td>
+                            <td>
+                              {landmark.builder.length > 0 ? (
+                                <Group>
+                                  {landmark.builder.map((u: any) => (
+                                    <Badge
+                                      key={u}
+                                      variant="outline"
+                                      onClick={() =>
+                                        handleRemoveBuilder(
+                                          landmark,
+                                          users.find(
+                                            (e: any) => e.username === u
+                                          ).uid
+                                        )
+                                      }
+                                    >
+                                      {u}
+                                    </Badge>
+                                  ))}
+                                </Group>
+                              ) : (
+                                "---"
+                              )}
+                            </td>
+                            <td>
+                              {landmark.requests.length > 0 ? (
+                                <Group>
+                                  {landmark.requests.map((u: any) => (
+                                    <Badge
+                                      key={u}
+                                      variant="outline"
+                                      onClick={() =>
+                                        handleAddBuilder(
+                                          landmark,
+                                          users.find(
+                                            (e: any) => e.username === u
+                                          ).uid
+                                        )
+                                      }
+                                    >
+                                      {u}
+                                    </Badge>
+                                  ))}
+                                </Group>
+                              ) : (
+                                "---"
+                              )}
+                            </td>
+                            <td>
+                              {landmark.requests.includes(user?.username) ? (
+                                <Button
+                                  color="red"
+                                  disabled={
+                                    landmark.completed ||
+                                    landmark.builder.length > 0
+                                  }
+                                  onClick={() => handleUnrequest(landmark)}
+                                >
+                                  Unapply
+                                </Button>
+                              ) : (
+                                <Button
+                                  color="green"
+                                  disabled={
+                                    landmark.completed ||
+                                    landmark.builder.length > 0
+                                  }
+                                  onClick={() => handleRequest(landmark)}
+                                >
+                                  Apply
+                                </Button>
+                              )}
+                            </td>
+                          </tr>
+                        ))
                     : null}
                 </tbody>
               </Table>
@@ -406,30 +536,58 @@ const LandmarksPage = () => {
               height="97%"
               style={{ zIndex: 10 }}
               zoom={12}
-              components={data?.map((landmark: any) => ({
-                type: "circle",
-                center: landmark.location,
-                options: {
-                  color:
-                    landmark.requests.length > 0
-                      ? landmark.builder.length > 0
-                        ? theme.colors.green[7]
-                        : theme.colors.cyan[7]
-                      : theme.colors.red[7], 
-                      opacity: selected
+              components={data
+                ?.filter((landmark: any) => {
+                  switch (statusFilter) {
+                    case 0:
+                      return (
+                        landmark.requests.length === 0 &&
+                        landmark.builder.length === 0 &&
+                        !landmark.completed
+                      );
+                    case 1:
+                      return (
+                        landmark.requests.length > 0 &&
+                        landmark.builder.length === 0 &&
+                        !landmark.completed
+                      );
+                    case 2:
+                      return landmark.builder.length > 0 && !landmark.completed;
+                    case 3:
+                      return landmark.completed;
+                    case 4:
+                      return (
+                        landmark.builder.includes(user.username) ||
+                        landmark.requests.includes(user.username)
+                      );
+                    default:
+                      return true;
+                  }
+                })
+                .map((landmark: any) => ({
+                  type: "circle",
+                  center: landmark.location,
+                  options: {
+                    color:
+                      landmark.requests.length > 0
+                        ? landmark.builder.length > 0
+                          ? theme.colors.green[7]
+                          : theme.colors.cyan[7]
+                        : theme.colors.red[7],
+                    opacity: selected
                       ? selected.id == landmark.id
                         ? 1
                         : 0.05
                       : 0.5,
-                },
-                radius: 15,
-                tooltip: landmark.name,
-                eventHandlers: {
-                  click: () => {
-                    setSelected(landmark);
                   },
-                },
-              }))}
+                  radius: 15,
+                  tooltip: landmark.name,
+                  eventHandlers: {
+                    click: () => {
+                      setSelected(landmark);
+                    },
+                  },
+                }))}
             />
           </Paper>
         </Grid.Col>
