5c2a7ee7431f5d1bf6e362be1e40598d38882797
Nudesuppe42 <jufo.theo@gmail.com>
2022-10-10 19:23:08 +0200

    ...

--- installDeps.sh
+++ installDeps.sh
@@ -8,4 +8,4 @@ cp /home/BuildTheEarth/MinefactProgress/frontend/node_modules/@minefactprogress/
 cp /home/BuildTheEarth/MinefactProgress/frontend/node_modules/@minefactprogress/schematicjs/src/schematic/mcedit/legacy.json /home/BuildTheEarth/MinefactProgress/frontend/node_modules/@minefactprogress/schematicjs/dist/esm/schematic/mcedit
 cp /home/BuildTheEarth/MinefactProgress/frontend/node_modules/@minefactprogress/schematicwebviewer/src/renderer/transparent.json /home/BuildTheEarth/MinefactProgress/frontend/node_modules/@minefactprogress/schematicwebviewer/dist/esm/renderer
 cp /home/BuildTheEarth/MinefactProgress/frontend/node_modules/@minefactprogress/schematicjs/src/schematic/mcedit/legacy.json /home/BuildTheEarth/MinefactProgress/frontend/node_modules/@minefactprogress/schematicjs/dist/cjs/schematic/mcedit
-
+cp /home/BuildTheEarth/MinefactProgress/frontend/node_modules/@minefactprogress/schematicwebviewer/src/renderer/nonOccluding.json /home/BuildTheEarth/MinefactProgress/frontend/node_modules/@minefactprogress/schematicwebviewer/dist/cjs/renderer
