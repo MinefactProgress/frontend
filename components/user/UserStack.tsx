e850f36fd24a0d7bd7d150429478d9ac2e92f532
Nudesuppe42 <jufo.theo@gmail.com>
2023-02-19 17:19:24 +0100

    landmarks

---
+++ components/user/UserStack.tsx
@@ -0,0 +1,68 @@
+import {
+  ActionIcon,
+  Avatar,
+  Group,
+  Menu,
+  ScrollArea,
+  Table,
+  Text,
+} from "@mantine/core";
+import {
+  IconDots,
+  IconMessages,
+  IconNote,
+  IconPencil,
+  IconReportAnalytics,
+  IconTrash,
+} from "@tabler/icons";
+
+interface UsersStackProps {
+  data: {
+    avatar: string;
+    name: string;
+    role: string;
+    data?: { text: string; description: string }[];
+    menu?: React.ReactElement;
+  }[];
+}
+
+export function UsersStack({ data }: UsersStackProps) {
+  const rows = data?.map((item) => (
+    <tr key={item.name}>
+      <td>
+        <Group spacing="sm">
+          <Avatar size={40} src={item.avatar} radius={40} />
+          <div>
+            <Text size="sm" weight={500}>
+              {item.name}
+            </Text>
+            <Text color="dimmed" size="xs">
+              {item.role}
+            </Text>
+          </div>
+        </Group>
+      </td>
+      {item.data?.map((d, i) => (
+        <td key={i}>
+          <Text size="sm">{d.text}</Text>
+          <Text size="xs" color="dimmed">
+            {d.description}
+          </Text>
+        </td>
+      ))}
+      <td>
+        <Group spacing={0} position="right">
+          {item.menu}
+        </Group>
+      </td>
+    </tr>
+  ));
+
+  return (
+    <ScrollArea>
+      <Table verticalSpacing="md">
+        <tbody>{rows}</tbody>
+      </Table>
+    </ScrollArea>
+  );
+}
--- pages/landmarks.tsx
+++ pages/landmarks.tsx
@@ -2,32 +2,42 @@ import {
   ActionIcon,
   Badge,
   Button,
-  Grid,
   Group,
-  MediaQuery,
-  Paper,
-  ScrollArea,
+  Modal,
   Select,
-  Table,
-  Tabs,
   Text,
   Tooltip,
   useMantineTheme,
 } from "@mantine/core";
+import {
+  IconBuildingMonument,
+  IconCheck,
+  IconCross,
+  IconSwitchVertical,
+} from "@tabler/icons";
+import useSWR, { mutate } from "swr";
 
 import { BackButton } from "../components/FastNavigation";
-import { IconBuildingMonument } from "@tabler/icons";
 import Map from "../components/map/Map";
 import { Page } from "../components/Page";
+import { Permissions } from "../util/permissions";
 import { ProgressCard } from "../components/Stats";
+import { UsersStack } from "../components/user/UserStack";
 import axios from "axios";
 import mapboxgl from "mapbox-gl";
-import useSWR from "swr";
+import { showNotification } from "@mantine/notifications";
+import { useRouter } from "next/router";
+import { useState } from "react";
+import useUser from "../hooks/useUser";
 
 const Landmarks = () => {
   const { data } = useSWR("/v1/landmarks");
   const { data: users } = useSWR("/v1/users");
+  const [user] = useUser();
   const theme = useMantineTheme();
+  const router = useRouter();
+  const [selected, setSelected] = useState<any>();
+  const [editOpen, setEditOpen] = useState(false);
 
   const requests: {
     name: any;
@@ -69,6 +79,7 @@ const Landmarks = () => {
       }
       // Requests
       for (const user of landmark.requests) {
+        console.log(user, landmark.name);
         if (requests.some((e: any) => e.name === user.user)) {
           requests.some((e: any) => {
             if (e.name === user.user) {
@@ -105,7 +116,6 @@ const Landmarks = () => {
     }
     return b.done - a.done;
   });
-
   const getLandmarkStatus = (landmark: any) => {
     if (!landmark) return 0;
     return !landmark.enabled
@@ -119,9 +129,293 @@ const Landmarks = () => {
       : 1;
   };
 
+  const handleRequest = (landmark: any, add?: boolean) => {
+    var requests = JSON.parse(landmark.requests);
+    if (add) {
+      requests.push({ user: user?.uid, priority: 3 });
+    } else {
+      requests = requests.filter((r: any) => r.user != user?.uid);
+    }
+    fetch(process.env.NEXT_PUBLIC_API_URL + `/v1/landmarks/${landmark.id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + user?.token,
+      },
+      body: JSON.stringify({
+        requests,
+      }),
+    })
+      .then((res) => res.json())
+      .then((res) => {
+        if (res.error) {
+          showNotification({
+            title: "Error requesting Landmark",
+            message: res.message,
+            color: "red",
+            icon: <IconCross />,
+          });
+        } else {
+          showNotification({
+            title: `${add ? "Unr" : "R"}equested Landmark`,
+            message:
+              `You ${add ? "" : "un"}requested the Landmark ` +
+              landmark.name +
+              "!",
+            color: "green",
+            icon: <IconCheck />,
+          });
+          mutate("/v1/landmarks");
+        }
+      });
+  };
+  const handleBuilder = (landmark: any, userID: number, add?: boolean) => {
+    if ((user?.permission || 0) < Permissions.moderator) return;
+    var requests = JSON.parse(landmark.requests);
+    var builder = JSON.parse(landmark.builder);
+    if (add) {
+      const newBuilder = requests.filter((r: any) => r.user == userID)[0];
+      requests = requests.filter((r: any) => r.user != userID);
+      builder.push(newBuilder);
+    } else {
+      const removedBuilder = builder.filter((r: any) => r.user == userID)[0];
+      builder = builder.filter((r: any) => r.user != userID);
+      requests.push(removedBuilder);
+    }
+
+    fetch(process.env.NEXT_PUBLIC_API_URL + `/v1/landmarks/${landmark.id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + user?.token,
+      },
+      body: JSON.stringify({
+        requests,
+        builder,
+      }),
+    })
+      .then((res) => res.json())
+      .then((res) => {
+        if (res.error) {
+          showNotification({
+            title: `Error ${add ? "adding" : "removing"} Builder`,
+            message: res.message,
+            color: "red",
+            icon: <IconCross />,
+          });
+        } else {
+          showNotification({
+            title: `Builder ${add ? "added" : "removed"}`,
+            message:
+              `Builder ${
+                add ? "added" : "removed"
+              } successfully for Landmark ` +
+              landmark.name +
+              "!",
+            color: "green",
+            icon: <IconCheck />,
+          });
+          mutate("/v1/landmarks");
+        }
+      });
+  };
+  const handlePriority = (landmark: any, e: any) => {
+    var requests = JSON.parse(landmark.requests).filter(
+      (r: any) => r.user != user?.uid
+    );
+    requests.push({ user: user?.uid, priority: e });
+
+    fetch(process.env.NEXT_PUBLIC_API_URL + `/v1/landmarks/${landmark.id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + user?.token,
+      },
+      body: JSON.stringify({
+        requests,
+      }),
+    })
+      .then((res) => res.json())
+      .then((res) => {
+        if (res.error) {
+          showNotification({
+            title: "Error updating priority",
+            message: res.message,
+            color: "red",
+            icon: <IconCross />,
+          });
+        } else {
+          showNotification({
+            title: "Updated priority",
+            message:
+              "You changed your priority of " + landmark.name + " to " + e,
+            color: "green",
+            icon: <IconCheck />,
+          });
+          mutate("/api/landmarks/get");
+        }
+      });
+  };
+
   return (
     <Page name="Landmarks" icon={<IconBuildingMonument />} noMargin>
       <div style={{ position: "relative", height: "100%", width: "100%" }}>
+        <Modal
+          size="md"
+          centered
+          opened={editOpen}
+          onClose={() => setEditOpen(false)}
+          title={
+            <>
+              {selected?.name + " "}
+              {!selected?.enabled ? (
+                <Badge color="grape">Not Available</Badge>
+              ) : selected?.done ? (
+                <Badge color="green">Done</Badge>
+              ) : selected?.requests != "[]" ? (
+                selected?.builder != "[]" ? (
+                  <Badge color="orange">Claimed</Badge>
+                ) : (
+                  <Badge color="cyan">Requested</Badge>
+                )
+              ) : (
+                <Badge color="red">Not Claimed</Badge>
+              )}
+            </>
+          }
+        >
+          {selected?.builder != "[]" && (
+            <>
+              <Text
+                color="dimmed"
+                size="xs"
+                transform="uppercase"
+                weight={700}
+                mb="xs"
+              >
+                Builder
+              </Text>
+              <UsersStack
+                data={
+                  selected?.builder &&
+                  JSON.parse(selected?.builder).map((u: any) => {
+                    const user = users.find((us: any) => us.uid == u.user);
+                    return {
+                      avatar: `https://mc-heads.net/avatar/${user.username}`,
+                      name: user.username,
+                      role: "Builder",
+                      data: [{ text: u.priority, description: "Priority" }],
+                      menu: (
+                        <Tooltip label="Change Role" withinPortal>
+                          <ActionIcon
+                            variant="outline"
+                            color="primary"
+                            onClick={() =>
+                              handleBuilder(selected, u.user, false)
+                            }
+                          >
+                            <IconSwitchVertical size={16} />
+                          </ActionIcon>
+                        </Tooltip>
+                      ),
+                    };
+                  })
+                }
+              />
+            </>
+          )}
+          {selected?.requests != "[]" && (
+            <>
+              <Text
+                color="dimmed"
+                size="xs"
+                transform="uppercase"
+                weight={700}
+                mb="xs"
+              >
+                Requests
+              </Text>
+              <UsersStack
+                data={
+                  selected?.requests &&
+                  JSON.parse(selected?.requests).map((u: any) => {
+                    const user = users.find((us: any) => us.uid == u.user);
+                    console.log(
+                      selected?.builder != "[]"
+                        ? JSON.parse(selected?.builder).some(
+                            (b: any) => user?.uid == b.user
+                          )
+                        : "truedd"
+                    );
+                    return {
+                      avatar: `https://mc-heads.net/avatar/${user.username}`,
+                      name: user.username,
+                      role: "Requester",
+                      data: [{ text: u.priority, description: "Priority" }],
+                      menu: (
+                        <Tooltip label="Change Role" withinPortal>
+                          <ActionIcon
+                            variant="outline"
+                            color="primary"
+                            onClick={() =>
+                              handleBuilder(selected, u.user, true)
+                            }
+                          >
+                            <IconSwitchVertical size={16} />
+                          </ActionIcon>
+                        </Tooltip>
+                      ),
+                    };
+                  })
+                }
+              />
+            </>
+          )}
+          <Group mt="md">
+            <Button
+              disabled={selected?.done || selected?.builder != "[]"}
+              onClick={() =>
+                handleRequest(
+                  selected,
+                  selected?.requests
+                    ? !JSON.parse(selected.requests).some(
+                        (d: any) => user?.uid == d.user
+                      )
+                    : true
+                )
+              }
+            >
+              {(
+                selected?.requests
+                  ? JSON.parse(selected.requests).some(
+                      (d: any) => user?.uid == d.user
+                    )
+                  : false
+              )
+                ? "Unrequest"
+                : "Request"}
+            </Button>
+            <Select
+              placeholder="Priority"
+              disabled={
+                !(selected?.requests
+                  ? JSON.parse(selected.requests).some(
+                      (d: any) => user?.uid == d.user
+                    )
+                  : true)
+              }
+              value={JSON.parse(selected?.requests || "[]")
+                .find((r: any) => user?.uid == r.user)
+                ?.priority.toString()}
+              onChange={(e: any) => handlePriority(selected, parseInt(e))}
+              data={[
+                { value: "1", label: "High" },
+                { value: "2", label: "Normal" },
+                { value: "3", label: "Low" },
+              ]}
+            />
+          </Group>
+        </Modal>
         <div
           style={{
             position: "absolute",
@@ -199,9 +493,8 @@ const Landmarks = () => {
                 layers: ["blocks-layer"],
               });
               if (selectedFeatures.length > 0) {
-                // TODO
-                //setEditBlock(selectedFeatures[0].properties);
-                //setEditOpen(true);
+                setSelected(selectedFeatures[0].properties);
+                setEditOpen(true);
               }
               // Set a filter matching selected features by FIPS codes
               // to activate the 'counties-highlighted' layer.
